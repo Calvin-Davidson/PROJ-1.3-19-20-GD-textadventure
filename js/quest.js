8eac4a0282cdf4b62ea27c240a588b6028764fa2
Calvin-Davidson <6a3x6bn4>
2020-03-18 13:05:21 +0100

    update

--- js/quest.js
+++ js/quest.js
@@ -3,16 +3,19 @@ const myInput = document.getElementById('myInput');
 const myOptions = document.getElementById('possibilities');
 const inv = document.getElementById('inv');
 const ErrorMSG = document.getElementById("error");
+const DescMSG = document.getElementById("beschrijving");
 
 let inventory = [];
 
+setInterval(update, 1)
 
 class room {
-    constructor(options, imagePath, items, requiredItem) {
+    constructor(options, imagePath, items, requiredItem, description = "") {
         this.options = options;
         this.image = imagePath;
         this.items = items;
         this.requiredItem = requiredItem;
+        this.beschrijving = description;
     }
 }
 
@@ -33,24 +36,24 @@ let grid = [
 
 let rooms = [];
 
-rooms[1] = new room(["down", "right"], "media/room1.png", [], "");
-rooms[2] = new room(["left", "right", "down"], "media/room2.png", ["key"], "");
-rooms[3] = new room(["left", "down"], "media/room3.png", [], "key");
-rooms[4] = new room(["up", "down", "right"], "media/room4.png", [], "");
-rooms[5] = new room(["left", "down", "right", "up"], "media/room5.png", [], "");
-rooms[6] = new room(["left", "down", "up"], "media/room6.png", [], "");
-rooms[7] = new room(["up", "right"], "media/room7.png", [], "");
-rooms[8] = new room(["left", "right", "up"], "media/room8.png", [], "");
-rooms[9] = new room(["left", "up", "floorup"], "media/room9.png", [], "");
-rooms[10] = new room(["down", "right"], "media/room10.png", [], "");
-rooms[11] = new room(["left", "right", "down"], "media/room11.png", [], "");
-rooms[12] = new room(["left", "down"], "media/room12.png", [], "");
-rooms[13] = new room(["up", "down", "right"], "media/room13.png", [], "");
-rooms[14] = new room(["left", "down", "right", "up"], "media/room14.png", [], "");
-rooms[15] = new room(["left", "down", "up"], "media/room15.png", [], "");
-rooms[16] = new room(["right", "up"], "media/room16.png", [], "");
-rooms[17] = new room(["left", "right", "up"], "media/room17.png", [], "");
-rooms[18] = new room(["left", "up", "floordown"], "media/room18.png", [], "");
+rooms[1] = new room(["down", "right"], "media/room1.png", [], "", "een mooie kamer");
+rooms[2] = new room(["left", "right", "down"], "media/room2.png", ["key"], "", "er is / was hier een sleutel!");
+rooms[3] = new room(["left", "down"], "media/room3.png", [], "key", "");
+rooms[4] = new room(["up", "down", "right"], "media/room4.png", [], "", "kamer 4 here we are!");
+rooms[5] = new room(["left", "down", "right", "up"], "media/room5.png", [], "", "");
+rooms[6] = new room(["left", "down", "up"], "media/room6.png", [], "", "");
+rooms[7] = new room(["up", "right"], "media/room7.png", [], "", "");
+rooms[8] = new room(["left", "right", "up"], "media/room8.png", [], "", "");
+rooms[9] = new room(["left", "up", "floorup"], "media/room9.png", [], "", "");
+rooms[10] = new room(["down", "right"], "media/room10.png", [], "", "");
+rooms[11] = new room(["left", "right", "down"], "media/room11.png", [], "", "");
+rooms[12] = new room(["left", "down"], "media/room12.png", [], "", "");
+rooms[13] = new room(["up", "down", "right"], "media/room13.png", [], "", "");
+rooms[14] = new room(["left", "down", "right", "up"], "media/room14.png", [], "", "");
+rooms[15] = new room(["left", "down", "up"], "media/room15.png", [], "", "");
+rooms[16] = new room(["right", "up"], "media/room16.png", [], "", "");
+rooms[17] = new room(["left", "right", "up"], "media/room17.png", [], "", "");
+rooms[18] = new room(["left", "up", "floordown"], "media/room18.png", [], "", "");
 
 let currentX = 0;
 let currentY = 0;
@@ -73,6 +76,9 @@ function update() {
     if (rooms[getPlayerRoom()].items.length != 0) {
         optionsMSG += "pickup ";
     }
+
+    DescMSG.innerHTML = rooms[getPlayerRoom()].beschrijving;
+
     myOptions.innerHTML = optionsMSG;
 
     // update inventory
@@ -80,13 +86,15 @@ function update() {
     for (let i = 0; i < inventory.length; i++) {
         items += "<li>" + inventory[i] + "</li>";
         if (i + 1 < inventory.length) {
-           items += " - "
+            items += " - "
         }
     }
 
     inv.innerHTML = items;
 }
+
 myInput.addEventListener('keydown', getInput, false);
+
 function getInput(e) {
     if (e.key == "Enter") {
         let inputArray = myInput.value.split(" ");
@@ -169,7 +177,7 @@ function getInput(e) {
                         if (!(inventory.includes(rooms[getPlayerRoom()].requiredItem))) {
                             currentX -= 1;
                             errorMSG("u heeft niet de juiste items om deze kamer in te gaan.");
-                            
+
                         } else {
                             inventory = inventory.filter(el => el !== rooms[getPlayerRoom()].requiredItem);
                         }
@@ -199,7 +207,7 @@ function getInput(e) {
 update();
 
 
-let errorMSG = function(msg) {
+let errorMSG = function (msg) {
     ErrorMSG.innerHTML = msg;
 
     setTimeout(function () {
